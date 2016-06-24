b4b0d4c5343ceae880fc5e8f9bbaf4e5f4239e25
John Lin <j.lin@prolificinteractive.com>
2016-06-24 11:36:58 -0400

    Initial commit

---
+++ TestReactiveCocoaIntsall/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  TestReactiveCocoaIntsall
+//
+//  Created by John Lin on 6/22/16.
+//  Copyright © 2016 John Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
+
